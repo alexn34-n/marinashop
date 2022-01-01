037b162bcf39ae80949126d23f3b31c5ee0f5e50
alexn34-n <alexn3469@mail.ru>
2022-01-01 15:41:50 +0300

    Добавлены Security  и отношения OneToMany между  User и Product.

---
+++ src/main/java/com/example/marinashop/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.marinashop.repositories;
+
+import com.example.marinashop.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
